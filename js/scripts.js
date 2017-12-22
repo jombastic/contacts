49e8e27cee03a0e0b6913a563c669a703f0030f4
Slavcho Mitrov <slavemitrov@yahoo.com>
2017-12-22 16:15:42 +0100

    Add functionality when user clicks on a contact to show its information in a modal

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,25 @@ $(function() {
 
     var rowNumber = $("#contactList").find("div.contactEntry").length;
 
-    $("#contactList").append("<div class='contactEntry'><strong>#" + rowNumber + "<span class='contactName'> " + name + " " + surname + "</span></strong><div class='hidden-data'><p class='address'" + address + "</p><p class='emailAddress'>" + emailAddress + "</p><p class='numberType'>" + numberType + "</p><p class='phoneNumber'>" + phoneNumber + "<p></div>");
+    $("#contactList").append("<div class='contactEntry'><strong>#" + rowNumber + "<span class='contactName'> " + name + " " + surname + "</span></strong><div class='hidden-data'><p class='address'>" + address + "</p><p class='emailAddress'>" + emailAddress + "</p><p class='numberType'>" + numberType + "</p><p class='phoneNumber'>" + phoneNumber + "<p></div>");
+
+    $(".contactEntry").click(function() {
+      var header = $(this).find(".contactName").text();
+      var modalAddress = $(this).find(".address").text();
+      var modalEmail = $(this).find(".emailAddress").text();
+      var modalNumType = $(this).find(".numberType").text();
+      var modalPhoneNum = $(this).find(".phoneNumber").text();
+
+      $(".modal-header h2").text(header);
+      $("#modalAddress").text(modalAddress);
+      $("#modalEmail").text(modalEmail);
+      $("#modalPhoneNum").text(modalPhoneNum);
+      $("#modalPhoneNum").append("<br/><small><i>" + modalNumType + "</i></small>");
+
+      $(".modal").show();
+      $(".close").click(function() {
+        $(".modal").hide();
+      });
+    });
   });
 });
